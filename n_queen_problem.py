139a6f76694b144cf3d51fbf6181ee563a9d7155
Mehmet Kaan ERKOÇ <mkaanerkoc@gmail.com>
2021-12-02 22:33:55 +0100

    Create n_queen_problem.py

---
+++ n_queen_problem.py
@@ -0,0 +1,45 @@
+'''
+Solution for N-queen problem. 
+It solves the N-queen problem recursively by searching through all possible paths.
+'''
+
+def calculate_legal_squares(size:int, queens:list, row:int) -> set:
+    '''
+    helper method. it calculates the legal squares for the 
+    given row by using board size and already placed queens 
+    on the board.
+    '''
+    line = set(range(size))
+    for queen in queens:
+        queen_row, queen_col = queen
+        depth = row - queen_row
+        line.discard(queen_col)
+        line.discard(queen_col-depth)
+        line.discard(queen_col+depth)
+    return line
+
+def number_of_combinations(size, queens=[], row=0) -> int:
+    '''
+    main solver method. it calculates all the legal squares
+    in the given row and recursively calling itself. it does 
+    depth-first-search by leveraging python list since it
+    can works like a stack with append and pop methods.
+    '''
+    legal_squares = calculate_legal_squares(size, queens, row)
+    
+    if row == (size-1): 
+        return len(legal_squares)
+    
+    number_of_paths = 0
+    for square in legal_squares:
+        ''' this is where graphical search is done '''
+        queens.append((row, square))
+        number_of_paths += number_of_combinations(size, queens, row+1)
+        queens.pop()
+    
+    return number_of_paths
+  
+# Main demonstration. Since other parameters are initialized as default, we can just pass the size parameter.
+result = number_of_combinations(size=8)
+print(result)
+
